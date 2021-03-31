ea34ce636d47229db834a3e1f002d485ab03872e
itamaredbmaestro <79922864+itamaredbmaestro@users.noreply.github.com>
2021-03-31 06:46:28 +0000

    Reset Team A

--- P1_1_Drop.sql
+++
@@ -1,8 +0,0 @@
-begin
-   execute immediate 'DROP PROCEDURE P1_1';
-exception when others then
-   if sqlcode != -4043 then
-      raise;
-   end if;
-end;
-/
--- P1_2_Drop.sql
+++
@@ -1,8 +0,0 @@
-begin
-   execute immediate 'DROP PROCEDURE P1_2';
-exception when others then
-   if sqlcode != -4043 then
-      raise;
-   end if;
-end;
-/
